5505d358b3a569db4317d284d5eba682d592fbc8
Remaster888 <125772401+Remaster888@users.noreply.github.com>
2023-02-19 17:43:15 -0500

    Update moduleloader.lua

--- moduleloader.lua
+++ moduleloader.lua
@@ -1,4 +1,5 @@
 getgenv().modules = modules or print("loading") or {
+    version = "0.0",
     status = "loading",
     files = {},
     latest = {
@@ -12,6 +13,11 @@ getgenv().modules = modules or print("loading") or {
         "pathfinding.lua"
     }
 }
+
+if not isfolder("remaster") then
+    makefolder("remaster")
+    writefile("remaster\\credits.txt", "written by iRay#1488")
+end
 
 for _, scriptName in next, modules.scripts do
     modules.files[scriptName] = game:HttpGet("https://raw.githubusercontent.com/Remaster888/hub/main/" .. scriptName, true)
@@ -27,13 +33,13 @@ modules.getModule = setmetatable({}, {__index = function(self, index)
         index = string.gsub(index, ".lua", "") .. ".lua"
         modules.status = "loading " .. index
         print("loading " .. index)
-        local file = ({pcall(readfile, index)})[2]; file = file and file ~= "file does not exist" and loadstring(file)()
+        local file = ({pcall(readfile, "remaster\\" .. index)})[2]; file = file and file ~= "file does not exist" and loadstring(file)()
 
         if not file or file.version ~= modules.latest[index] then
             modules.status = not file and "downloading " .. index or "updating " .. index
             print(modules.status)
             file = modules.files[index]
-            writefile(index, file)
+            writefile("remaster\\" .. index, file)
             file = loadstring(file)()
         end
 
