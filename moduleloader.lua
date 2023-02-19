89dcdd223a6d43488cfc37bf00d88dbd9d7d6d52
Remaster888 <125772401+Remaster888@users.noreply.github.com>
2023-02-19 10:52:30 -0500

    Update moduleloader.lua

--- moduleloader.lua
+++ moduleloader.lua
@@ -1,1 +1,44 @@
+getgenv().modules = modules or print("loading") or {
+    status = "loading",
+    latest = {
+        ["mainscript.lua"] = "0.0",
+        ["pathfinding.lua"] = "0.0"
+    },
+    scripts = {
+        "mainscript.lua",
+        "pathfinding.lua"
+    }
+}
 
+local files = {}
+for _, scriptName in next, modules.scripts do
+    files[scriptName] = game:HttpGet("https://raw.githubusercontent.com/Remaster888/hub/main/" .. scriptName, true)
+end
+
+modules.getModule = setmetatable({}, {__index = function(self, index)
+    local startIndex = index
+    local value = rawget(self, index)
+
+    if value then
+        return value
+    else
+        index = string.gsub(index, ".lua", "") .. ".lua"
+        modules.status = "loading " .. index
+        print("loading " .. index)
+        local file = ({pcall(readfile, index)})[2]; file = file and file ~= "file does not exist" and loadstring(file)()
+
+        if not file or file.version ~= modules.latest[index] then
+            modules.status = not file and "downloading " .. index or "updating " .. index
+            print(modules.status)
+            file = files[index]
+            writefile(index, file)
+            file = loadstring(file)()
+        end
+
+        modules.status = "loading"
+        rawset(self, startIndex, file)
+        return file
+    end
+end})
+
+return modules
