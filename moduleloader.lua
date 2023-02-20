02f71a4ccba2a781e82a36012e3e5302b8101d48
Remaster888 <125772401+Remaster888@users.noreply.github.com>
2023-02-20 08:37:31 -0500

    Update moduleloader.lua

--- moduleloader.lua
+++ moduleloader.lua
@@ -38,6 +38,7 @@ modules.getModule = setmetatable({}, {__index = function(self, index)
         print("loading " .. index)
         local file = ({pcall(readfile, "remaster\\" .. index)})[2]; file = file and file ~= "file does not exist" and loadstring(file)()
 
+        pcall(function()
         if not file or file.version ~= modules.latest[index] then
             modules.status = not file and "downloading " .. index or "updating " .. index
             print(modules.status)
@@ -45,6 +46,7 @@ modules.getModule = setmetatable({}, {__index = function(self, index)
             writefile("remaster\\" .. index, file)
             file = loadstring(file)()
         end
+        end)
 
         modules.status = "loading"
         rawset(self, startIndex, file)
