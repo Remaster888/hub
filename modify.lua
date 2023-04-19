459310bb080fb39e3e5f89cd0001d7e7183d4b34
Remaster888 <125772401+Remaster888@users.noreply.github.com>
2023-04-19 17:10:47 -0400

    Update modify.lua

--- modify.lua
+++ modify.lua
@@ -1,5 +1,45 @@
 local modify = {
-    version = "0.0"
+    version = "0.0",
+    connections = {},
+    upvals = {},
+    hooks = {}
 }
+
+function modify.addConnection(self, object, event)
+    table.insert(self.connections, object:Connect(event))
+end
+
+function modify.hookUpValue(self, func, index, value)
+    local old = debug.getupvalue(func, index)
+    debug.setupvalue(func, index, value)
+    table.insert(self.hooks, {func, index, old})
+end
+
+function modify.hookFunction(self, item, index, func)
+    local old = item[index]
+    hookfunction(item[index], func)
+    --item[index] = func
+
+    table.insert(self.hooks, {item, index, old})
+end
+
+function modify.unload()
+    for _, connection in next, modify.connections do
+        connection:Disconnect()
+    end
+
+    for _, hookData in next, modify.upvals do
+        debug.setupvalue(hookData[1], hookData[2], hookData[3])
+    end
+
+    for _, hookData in next, modify.hooks do
+        hookData[1][hookData[2] ] = hookData[3]
+    end
+
+    --for _, drawing in next, modules:getModule("userinterface").drawings do
+    --    drawing.Visible = false
+    --    drawing:Remove()
+    --end
+end
 
 return modify
